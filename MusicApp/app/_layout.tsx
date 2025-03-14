073142a0354cd8aad3807f85a44fa0bf60f67e5b
BellaFred <156227097+BellaFred@users.noreply.github.com>
2025-03-14 15:08:09 -0500

    Authorization Implementation

--- MusicApp/app/_layout.tsx
+++ MusicApp/app/_layout.tsx
@@ -1,10 +1,12 @@
 import { Stack } from "expo-router";
-
+import { SessionProvider } from "../store/auth/authContext";
 export default function RootLayout() {
   return (
+    <SessionProvider>
     <Stack initialRouteName="index">
       <Stack.Screen name="index" />
       <Stack.Screen name="(tabs)" options={{ headerShown:false }} />
     </Stack>
+    </SessionProvider>
   );
 }
--- MusicApp/firebaseConfig.ts
+++ MusicApp/firebaseConfig.ts
@@ -1,16 +1,8 @@
-// Import the functions you need from the SDKs you need
 import { initializeApp } from "firebase/app";
-import { getAnalytics } from "firebase/analytics";
-import {getAuth} from 'firebase/auth';
-// import {...} from 'firebase/database';
-// import {...} from 'firebase/firestore';
-// import {...} from 'firebase/functions';
-// import {...} from 'firebase/storage';
-// TODO: Add SDKs for Firebase products that you want to use
-// https://firebase.google.com/docs/web/setup#available-libraries
+import { initializeAuth, getReactNativePersistence } from "firebase/auth";
+import ReactNativeAsyncStorage from "@react-native-async-storage/async-storage";
+import { Platform } from "react-native";
 
-// Your web app's Firebase configuration
-// For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
   apiKey: "AIzaSyBBQVC3VGLGF6TdYjIbB2NogGAkdEWMvL8",
   authDomain: "music-app-65dfe.firebaseapp.com",
@@ -23,7 +15,8 @@ const firebaseConfig = {
 
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
-const analytics = getAnalytics(app);
-export const auth = getAuth(app);
-//export const db = getFirestore(app);
-//export const storage = getStorage(app);
+const auth = initializeAuth(app, {
+  persistence: getReactNativePersistence(ReactNativeAsyncStorage)
+});
+
+export { auth, app };
