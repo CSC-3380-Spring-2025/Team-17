90dcc6c24e1e2a00eae83f80bd0858090ce23c88
BellaFred <156227097+BellaFred@users.noreply.github.com>
2025-03-15 20:10:22 -0500

    Revert "Authorization Implementation"
    This reverts commit 073142a0354cd8aad3807f85a44fa0bf60f67e5b.

--- MusicApp/app/_layout.tsx
+++ MusicApp/app/_layout.tsx
@@ -1,12 +1,10 @@
 import { Stack } from "expo-router";
-import { SessionProvider } from "../store/auth/authContext";
+
 export default function RootLayout() {
   return (
-    <SessionProvider>
     <Stack initialRouteName="index">
       <Stack.Screen name="index" />
       <Stack.Screen name="(tabs)" options={{ headerShown:false }} />
     </Stack>
-    </SessionProvider>
   );
 }
